f70050fe895367b1c5610ada343991cff902e65a
dev-vicky-29 <vickydeshmukh2509@gmail.com>
2024-01-17 20:19:35 +0530

    Create Pattern.java

---
+++ Basics of Programming/28 December 2023/Pattern.java
@@ -0,0 +1,9 @@
+package Basics of Programming;
+public class Pattern{
+    public static void main(String args){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
